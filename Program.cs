4d296ed6ca55798a3c7c662215012da826335379
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-23 15:48:01 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _03.ArgumentExceptionTutorial
+{
+    class Program
+    {
+
+        static void Divide(int a) 
+        {
+            if (a == 10)
+            {
+                throw new ArgumentException("Ten is not allowed");
+                
+            }
+            int result = 10 / a;
+        }
+        static void ReadNumber() 
+        {
+            int a = int.Parse(Console.ReadLine());
+            Divide(a);
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                ReadNumber();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
